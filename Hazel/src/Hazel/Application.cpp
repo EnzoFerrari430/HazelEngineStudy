e32209513b177ba211622ed39df4a9b0f9f540cc
EnzoFerrari430 <782794844@qq.com>
2023-01-10 13:34:33 +0800

    测试 Input Polling以及格式化tab替换成4空格

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -5,80 +5,84 @@
 
 #include <glad/glad.h>
 
+#include "Hazel/Input.h"
+
 namespace Hazel {
 
 //#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
-	Application* Application::s_Instance = nullptr;
+    Application* Application::s_Instance = nullptr;
 
-	Application::Application()
-	{
-		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
-		s_Instance = this;
+    Application::Application()
+    {
+        HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
+        s_Instance = this;
 
-		m_Window = std::unique_ptr<Window>(Window::Create());
-		m_Window->SetEventCallback(HZ_BIND_EVENT_FN(Application::OnEvent));
-	}
-	
-	Application::~Application()
-	{
-	}
+        m_Window = std::unique_ptr<Window>(Window::Create());
+        m_Window->SetEventCallback(HZ_BIND_EVENT_FN(Application::OnEvent));
+    }
+    
+    Application::~Application()
+    {
+    }
 
-	void Application::PushLayer(Layer* layer)
-	{
-		m_Layerstack.PushLayer(layer);
-		layer->OnAttach();
-	}
+    void Application::PushLayer(Layer* layer)
+    {
+        m_Layerstack.PushLayer(layer);
+        layer->OnAttach();
+    }
 
-	void Application::PushOverlay(Layer* layer)
-	{
-		m_Layerstack.PushOverlay(layer);
-		layer->OnAttach();
-	}
+    void Application::PushOverlay(Layer* layer)
+    {
+        m_Layerstack.PushOverlay(layer);
+        layer->OnAttach();
+    }
 
-	//事件集中调度
-	void Application::OnEvent(Event& event)
-	{
-		EventDispatcher dispatcher(event);
-		dispatcher.Dispatch<WindowCloseEvent>(HZ_BIND_EVENT_FN(Application::OnWindowClose));
+    //事件集中调度
+    void Application::OnEvent(Event& event)
+    {
+        EventDispatcher dispatcher(event);
+        dispatcher.Dispatch<WindowCloseEvent>(HZ_BIND_EVENT_FN(Application::OnWindowClose));
 
-		HZ_CORE_INFO("{0}", event);
+        HZ_CORE_INFO("{0}", event);
 
-		//事件处理，从后往前
-		for (auto it = m_Layerstack.end(); it != m_Layerstack.begin();)
-		{
-			(*--it)->OnEvent(event);
-			if (event.Handled)
-				break;
-		}
-	}
+        //事件处理，从后往前
+        for (auto it = m_Layerstack.end(); it != m_Layerstack.begin();)
+        {
+            (*--it)->OnEvent(event);
+            if (event.Handled)
+                break;
+        }
+    }
 
-	/*
-	TODO:
-	这里要对逻辑和渲染进行区分
-	OnLogical
-	OnRender
-	两个步骤
-	*/
-	void Application::Run()
-	{
-		while (m_Running)
-		{
-			glClearColor(1.0f, 0.0f, 1.0f, 0.33f);
-			glClear(GL_COLOR_BUFFER_BIT);
-			
-			//图像渲染 从前往后
-			for (Layer* layer : m_Layerstack)
-				layer->OnUpdate();
+    /*
+    TODO:
+    这里要对逻辑和渲染进行区分
+    OnLogical
+    OnRender
+    两个步骤
+    */
+    void Application::Run()
+    {
+        while (m_Running)
+        {
+            glClearColor(1.0f, 0.0f, 1.0f, 0.33f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            
+            //图像渲染 从前往后
+            for (Layer* layer : m_Layerstack)
+                layer->OnUpdate();
+            
+            auto[x, y] = Input::GetMousePosition();
+            HZ_CORE_TRACE("{0}, {1}", x, y);
+            m_Window->OnUpdate();
+        }
+    }
 
-			m_Window->OnUpdate();
-		}
-	}
-
-	bool Application::OnWindowClose(WindowCloseEvent& e)
-	{
-		m_Running = false;
-		return true;
-	}
+    bool Application::OnWindowClose(WindowCloseEvent& e)
+    {
+        m_Running = false;
+        return true;
+    }
 
 }
--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -3,39 +3,39 @@
 class ExampleLayer : public Hazel::Layer
 {
 public:
-	ExampleLayer()
-		: Layer("Example")
-	{
+    ExampleLayer()
+        : Layer("Example")
+    {
 
-	}
+    }
 
-	void OnUpdate() override
-	{
-		HZ_INFO("ExampleLayer::Update");
-	}
+    void OnUpdate() override
+    {
+        //HZ_INFO("ExampleLayer::Update");
+    }
 
-	void OnEvent(Hazel::Event& event) override
-	{
-		HZ_TRACE("{0}", event);
-	}
+    void OnEvent(Hazel::Event& event) override
+    {
+        //HZ_TRACE("{0}", event);
+    }
 };
 
 class SandBox : public Hazel::Application
 {
 public:
-	SandBox()
-	{
-		PushLayer(new ExampleLayer());
-		PushOverlay(new Hazel::ImGuiLayer());
-	}
+    SandBox()
+    {
+        PushLayer(new ExampleLayer());
+        PushOverlay(new Hazel::ImGuiLayer());
+    }
 
-	~SandBox()
-	{
+    ~SandBox()
+    {
 
-	}
+    }
 };
 
 Hazel::Application* Hazel::CreateApplication()
 {
-	return new SandBox();
+    return new SandBox();
 }
