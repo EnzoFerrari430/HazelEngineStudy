b76adbd142cdb6c6b633b1bf348c878f04f93767
EnzoFerrari430 <782794844@qq.com>
2023-02-08 15:06:37 +0800

    Vertex Buffer Layouts

--- Hazel/src/Platform/OpenGL/OpenGLBuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,10 +10,14 @@ namespace Hazel {
         OpenGLVertexBuffer(float* vertices, uint32_t size);
         virtual ~OpenGLVertexBuffer();
 
-        virtual void Bind() const override;
-        virtual void Unbind() const override;
+        void Bind() const override;
+        void Unbind() const override;
+
+        void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+        const BufferLayout& GetLayout() const override { return m_Layout; }
     private:
         uint32_t m_RendererID;
+        BufferLayout m_Layout; 
     };
 
     class OpenGLIndexBuffer : public IndexBuffer
@@ -22,10 +26,12 @@ namespace Hazel {
         OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
         virtual ~OpenGLIndexBuffer();
 
-        virtual void Bind() const override;
-        virtual void Unbind() const override;
+        void Bind() const override;
+        void Unbind() const override;
 
-        virtual uint32_t GetCount() const override { return m_Count; }
+
+
+        uint32_t GetCount() const override { return m_Count; }
     private:
         uint32_t m_RendererID;
         uint32_t m_Count;
