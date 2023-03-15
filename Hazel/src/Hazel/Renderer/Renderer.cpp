0a0df5600bea8a838c96edec7b48c601998ad6c8
EnzoFerrari430 <782794844@qq.com>
2023-03-15 23:06:13 +0800

    Update Shader Abstraction and Uniforms

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -1,6 +1,8 @@
 #include "hzpch.h"
 #include "Renderer.h"
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 namespace Hazel {
 
     Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
@@ -17,8 +19,10 @@ namespace Hazel {
     void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
     {
         shader->Bind();
-        shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);  //这个每一帧每一个shader只用设置一次 应该放在全局变量中
-        shader->UploadUniformMat4("u_Transform", transform);  //每个模型都要设置一次
+
+        // TODO: 这里暂时只使用OpenGL, 后期抽象要考虑其他图形接口 DirectX Vulcan等
+        std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);  //这个每一帧每一个shader只用设置一次 应该放在全局变量中
+        std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);  //每个模型都要设置一次
 
         vertexArray->Bind();
         RenderCommand::DrawIndexed(vertexArray);
--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -1,6 +1,11 @@
 #include <Hazel.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
+#include <ImGui/imgui.h>
+
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 class ExampleLayer : public Hazel::Layer
@@ -64,7 +69,7 @@ public:
             }
         )";
 
-        m_Shader.reset(new Hazel::Shader(vertexSrc, fragmentSrc));
+        m_Shader.reset(Hazel::Shader::Create(vertexSrc, fragmentSrc));
 
 
 
@@ -112,15 +117,15 @@ public:
 
             in vec3 v_Position;
             out vec4 color;
-            uniform vec4 u_Color;
+            uniform vec3 u_Color;
 
             void main()
             {
-                color = u_Color;
+                color = vec4(u_Color, 1.0f);
             }
         )";
 
-        m_FlatColorShader.reset(new Hazel::Shader(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
+        m_FlatColorShader.reset(Hazel::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
     }
 
     void OnUpdate(Hazel::Timestep ts) override
@@ -168,18 +173,15 @@ public:
         glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
         glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
 
+        std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();//先绑定，否则(x = 0, y = 0)方块在第一帧时设置uniform失败
+        std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
 
-        m_FlatColorShader->Bind();//先绑定，否则(x = 0, y = 0)方块在第一帧时设置uniform失败
         for (int y = 0; y < 20; ++y)
         {
             for (int x = 0; x < 20; ++x)
             {
                 glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
                 glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_SquarePosition + pos) * scale; //矩阵右乘，一般先缩放，再旋转，最后位移
-                if (x % 2 == 0)
-                    m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
-                else
-                    m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
                 Hazel::Renderer::Submit(m_FlatColorShader, m_FlatColorVertexArray, transform);
             }
         }
@@ -191,7 +193,9 @@ public:
 
     void OnImGuiRender() override
     {
-
+        ImGui::Begin("Settings");
+        ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+        ImGui::End();
     }
 
 private:
@@ -210,6 +214,8 @@ private:
 
     glm::vec3 m_SquarePosition;
     float m_SquareMoveSpeed = 1.0f;
+
+    glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class SandBox : public Hazel::Application
