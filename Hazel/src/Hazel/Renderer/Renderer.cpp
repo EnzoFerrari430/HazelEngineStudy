2ee843c5942bcd400bb37511883afbb060dfb8f9
EnzoFerrari430 <782794844@qq.com>
2023-03-15 09:20:31 +0800

    update Transform
    保留JKLI控制功能

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -14,10 +14,11 @@ namespace Hazel {
     {
     }
 
-    void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+    void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
     {
         shader->Bind();
-        shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+        shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);  //这个每一帧每一个shader只用设置一次 应该放在全局变量中
+        shader->UploadUniformMat4("u_Transform", transform);  //每个模型都要设置一次
 
         vertexArray->Bind();
         RenderCommand::DrawIndexed(vertexArray);
--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -1,12 +1,14 @@
 #include <Hazel.h>
 
+#include <glm/gtc/matrix_transform.hpp>
+
 
 class ExampleLayer : public Hazel::Layer
 {
 public:
     ExampleLayer()
         : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f, -1.0f, 1.0f)
-        , m_CameraPosition(0.0f)
+        , m_CameraPosition(0.0f), m_SquarePosition(0.0f)
     {
         m_VertexArray.reset(Hazel::VertexArray::Create());
         float vertices[3 * 7] = {
@@ -35,6 +37,7 @@ public:
             layout(location = 1) in vec4 a_Color;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             out vec3 v_Position;
             out vec4 v_Color;
@@ -43,7 +46,7 @@ public:
             {
                 v_Position = a_Position;
                 v_Color = a_Color;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -69,10 +72,10 @@ public:
 
         m_BlueVertexArray.reset(Hazel::VertexArray::Create());
         float blueVertices[3 * 4] = {
-            -0.75f, -0.75f, 0.0f,
-             0.75f, -0.75f, 0.0f,
-             0.75f,  0.75f, 0.0f,
-            -0.75f,  0.75f, 0.0f
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.5f,  0.5f, 0.0f,
+            -0.5f,  0.5f, 0.0f
         };
         std::shared_ptr<Hazel::VertexBuffer> blueVertexBuffer;
         blueVertexBuffer.reset(Hazel::VertexBuffer::Create(blueVertices, sizeof(blueVertices)));
@@ -93,13 +96,14 @@ public:
             layout(location = 0) in vec3 a_Position;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             out vec3 v_Position;
 
             void main()
             {
                 v_Position = a_Position;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -124,21 +128,29 @@ public:
 
         if (Hazel::Input::IsKeyPressed(HZ_KEY_LEFT))
             m_CameraPosition.x -= m_CameraMoveSpeed * ts;
-
-        if (Hazel::Input::IsKeyPressed(HZ_KEY_RIGHT))
+        else if (Hazel::Input::IsKeyPressed(HZ_KEY_RIGHT))
             m_CameraPosition.x += m_CameraMoveSpeed * ts;
-
-        if (Hazel::Input::IsKeyPressed(HZ_KEY_DOWN))
-            m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
         if (Hazel::Input::IsKeyPressed(HZ_KEY_UP))
             m_CameraPosition.y += m_CameraMoveSpeed * ts;
+        else if (Hazel::Input::IsKeyPressed(HZ_KEY_DOWN))
+            m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
         if (Hazel::Input::IsKeyPressed(HZ_KEY_A))
             m_CameraRotation -= m_CameraRotationSpeed * ts;
-
-        if (Hazel::Input::IsKeyPressed(HZ_KEY_D))
+        else if (Hazel::Input::IsKeyPressed(HZ_KEY_D))
             m_CameraRotation += m_CameraRotationSpeed * ts;
+
+
+        if (Hazel::Input::IsKeyPressed(HZ_KEY_J))
+            m_SquarePosition.x -= m_SquareMoveSpeed * ts;
+        else if (Hazel::Input::IsKeyPressed(HZ_KEY_L))
+            m_SquarePosition.x += m_SquareMoveSpeed * ts;
+
+        if (Hazel::Input::IsKeyPressed(HZ_KEY_I))
+            m_SquarePosition.y += m_SquareMoveSpeed * ts;
+        else if (Hazel::Input::IsKeyPressed(HZ_KEY_K))
+            m_SquarePosition.y -= m_SquareMoveSpeed * ts;
 
 
         Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
@@ -149,7 +161,16 @@ public:
         m_Camera.SetRotation(m_CameraRotation);
 
         Hazel::Renderer::BeginScene(m_Camera);
-        Hazel::Renderer::Submit(m_BlueShader, m_BlueVertexArray);
+
+        for (int y = 0; y < 20; ++y) {
+            for (int x = 0; x < 20; ++x) {
+                glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+                glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+                glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_SquarePosition + pos) * scale; //矩阵右乘，一般先缩放，再旋转，最后位移
+                Hazel::Renderer::Submit(m_BlueShader, m_BlueVertexArray, transform);
+            }
+        }
+
         Hazel::Renderer::Submit(m_Shader, m_VertexArray);
         Hazel::Renderer::EndScene();
 
@@ -173,6 +194,9 @@ private:
 
     float m_CameraRotation = 0.0f;
     float m_CameraRotationSpeed = 30.0f;
+
+    glm::vec3 m_SquarePosition;
+    float m_SquareMoveSpeed = 1.0f;
 };
 
 class SandBox : public Hazel::Application
