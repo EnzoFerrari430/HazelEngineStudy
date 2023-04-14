8a9f2b4be07e99804dcb632df1af8412f3689be8
EnzoFerrari430 <782794844@qq.com>
2023-04-14 13:20:57 +0800

    Update Single Shader 2D Renderer

--- SandBox/assets/shaders/Texture.glsl
+++ SandBox/assets/shaders/Texture.glsl
@@ -23,8 +23,9 @@ layout(location = 0) out vec4 color;
 in vec2 v_TexCoord;
 
 uniform sampler2D u_Texture;//default value: 0
+uniform vec4 u_Color;
 
 void main()
 {
-    color = texture(u_Texture, v_TexCoord * 10.0f) * vec4(v_TexCoord.r, v_TexCoord.g, 0.0f, 1.0f);
+    color = texture(u_Texture, v_TexCoord * 10.0f) * u_Color;
 }
