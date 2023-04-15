c8e52e0b824af979079d3793779569c0890c2dd8
EnzoFerrari430 <782794844@qq.com>
2023-04-15 13:46:38 +0800

    code format

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -24,9 +24,9 @@ namespace Hazel {
     {
         None = 0,
         EventCategoryApplication        = BIT(0),
-        EventCategoryInput                = BIT(1),
-        EventCategoryKeyboard            = BIT(2),
-        EventCategoryMouse                = BIT(3),
+        EventCategoryInput              = BIT(1),
+        EventCategoryKeyboard           = BIT(2),
+        EventCategoryMouse              = BIT(3),
         EventCategoryMouseButton        = BIT(4)
     };
 
