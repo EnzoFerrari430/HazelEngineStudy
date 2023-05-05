2e6e5e92839c39fa0272077bd7a598016980561b
EnzoFerrari430 <782794844@qq.com>
2023-05-05 14:25:28 +0800

    Update Renderer Stats and Batch Improvements

--- SandBox/src/Sandbox2D.cpp
+++ SandBox/src/Sandbox2D.cpp
@@ -25,9 +25,12 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 {
+    PROFILE_SCOPE("Sandbox2D::OnUpdate");
     HZ_PROFILE_FUNCTION();
 
     m_CameraController.OnUpdate(ts);
+
+    Hazel::Renderer2D::ResetStats();
 
     {
         HZ_PROFILE_SCOPE("Renderer Prep");
@@ -44,8 +47,21 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
         Hazel::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, -30.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
         Hazel::Renderer2D::DrawQuad({ -0.5f, -0.5f }, { 0.5f, 0.5f }, { 0.2f, 0.3f, 0.8f, 1.0f });
         Hazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 1.5f, 0.75f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-        Hazel::Renderer2D::DrawQuad({ -0.0f, -0.0f, -0.5f }, { 10.0f, 10.0f }, m_BoxTexture, 10.0f, glm::vec4(1.0f, 0.8f, 0.2f, 1.0f));
+        Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.5f }, { 10.0f, 10.0f }, m_BoxTexture, 20.0f, glm::vec4(1.0f, 0.8f, 0.2f, 1.0f));
         Hazel::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, -0.1f }, { 1.0f, 1.0f }, rotation, m_BoxTexture, 20.0f, glm::vec4(1.0f, 0.8f, 0.2f, 1.0f));
+        Hazel::Renderer2D::EndScene();
+        
+        Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+
+        for (float y = -5.0f; y <= 5.0f; y += 0.1f)
+        {
+            for (float x = -5.0f; x <= 5.0f; x += 0.1f)
+            {
+                glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.4f };
+                Hazel::Renderer2D::DrawQuad({ x, y }, { 0.05f, 0.05f }, color);
+            }
+        }
+
         Hazel::Renderer2D::EndScene();
     }
 }
@@ -54,6 +70,23 @@ void Sandbox2D::OnImGuiRender()
 {
     HZ_PROFILE_FUNCTION();
     ImGui::Begin("Settings");
+
+    auto stats = Hazel::Renderer2D::GetStats();
+    ImGui::Text("Renderer2D Stats:");
+    ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+    ImGui::Text("Quads: %d", stats.QuadCount);
+    ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+    ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
+    for (auto& result : m_ProfileResults)
+    {
+        char label[50];
+        strcpy(label, "%.01f FPS ");
+        strcat(label, result.Name);
+        ImGui::Text(label, 1000/result.Time);
+    }
+    m_ProfileResults.clear();
+
     ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
     ImGui::End();
 }
--- SandBox/src/Sandbox2D.h
+++ SandBox/src/Sandbox2D.h
@@ -2,6 +2,43 @@
 
 #include <Hazel.h>
 
+template<typename Fn>
+class Timer
+{
+public:
+    Timer(const char* name, Fn&& func)
+        : m_Name(name), m_Func(func), m_Stopped(false)
+    {
+        m_StartTimepoint = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        if (!m_Stopped)
+            Stop();
+    }
+
+    void Stop()
+    {
+        auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+        m_Stopped = true;
+
+        float duration = (end - start) * 0.001f;
+        m_Func({ m_Name, duration });
+    }
+private:
+    const char* m_Name;
+    std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+    bool m_Stopped;
+    Fn m_Func;
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) {m_ProfileResults.push_back(profileResult); })
+
 class Sandbox2D : public Hazel::Layer
 {
 public:
@@ -22,4 +59,12 @@ private:
     Hazel::Ref<Hazel::Texture2D> m_BoxTexture;
 
     glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+
+    // benckmark
+    struct ProfileResult
+    {
+        const char* Name;
+        float Time;
+    };
+    std::vector<ProfileResult> m_ProfileResults;
 };
