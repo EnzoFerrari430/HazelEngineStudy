4eb54c7e69a1a105f0e7a056674b384431c4d223
EnzoFerrari430 <782794844@qq.com>
2024-05-06 15:50:59 +0800

    Add Properties panel

--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -4,6 +4,7 @@
 #include "Hazel/Scene/Components.h"
 
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Hazel {
 
@@ -25,6 +26,15 @@ namespace Hazel {
             Entity entity{ entityID , m_Context.get() };
             DrawEntityNode(entity);
         }
+
+        if (ImGui::IsMouseDown(ImGuiMouseButton_Left) && ImGui::IsWindowHovered())
+            m_SelectionContext = {};
+
+        ImGui::End();
+
+        ImGui::Begin("Properties");
+        if (m_SelectionContext)
+            DrawComponent(m_SelectionContext);
 
         ImGui::End();
     }
@@ -50,6 +60,34 @@ namespace Hazel {
                 ImGui::TreePop();
             }
             ImGui::TreePop();
+        }
+    }
+
+    void SceneHierarchyPanel::DrawComponent(Entity entity)
+    {
+        if (entity.HasComponent<TagComponent>())
+        {
+            auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+
+            char buffer[256];
+            memset(buffer, 0, sizeof(buffer));
+            strcpy(buffer, tag.c_str());
+            if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+            {
+                tag = std::string(buffer);
+            }
+        }
+
+        if (entity.HasComponent<TransformComponent>())
+        {
+            if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+            {
+                auto& transform = entity.GetComponent<TransformComponent>().Transform;
+                ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+                ImGui::TreePop();
+            }
         }
     }
 
