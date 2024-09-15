9c1279cd84491ccf5f9728d3686947e274d27939
EnzoFerrari430 <782794844@qq.com>
2024-09-15 15:15:12 +0800

    1.Added New Open SaveAs file menu options to Hazelnut 2.Added Win32 Open/Save file dialogs

--- Hazelnut/src/EditorLayer.h
+++ Hazelnut/src/EditorLayer.h
@@ -56,6 +56,12 @@ namespace Hazel {
         void OnImGuiRender() override;
         void OnEvent(Event& e) override;
     private:
+        bool OnKeyPressed(KeyPressedEvent& e);
+
+        void NewScene();
+        void OpenScene();
+        void SaveSceneAs();
+    private:
         OrthographicCameraController m_CameraController;
 
         Ref<Texture2D> m_SpriteSheet;
